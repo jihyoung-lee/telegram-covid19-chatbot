0773f74a4f44741e328cc651cc6ba553c0b2aeee
J.H.Lee <67559886+jihyoung-lee@users.noreply.github.com>
2020-12-29 23:28:57 +0900

    Update data.py
    1. 시간형식 추가
    2. 누적 확진자 추가

--- telegram_Chatbot/data.py
+++ telegram_Chatbot/data.py
@@ -9,24 +9,39 @@ import datetime
 
 # 현재시간
 now = datetime.datetime.now()
-nowdate = now.strftime('%Y%m%d')
+nowdate = now.strftime('%Y%m%d')  # 시간형식 변경
+nowdate2 = now.strftime('%Y년 %m월 %d일')
+# api 요청
 
-# api요청
-
-ServiceKey = 'vF9w%2FEtQmlLZaLXPV6ZrrOzZtZuMl9p0hGmz4PFW6DDnUGw0x4ob6HzuYwVct3EdVEbpxbdAEJukvJluA56gjA%3D%3D'
+ServiceKey = ''
 pageNo = 1
 numOfRows = 1
 startCreateDt = int(nowdate)
-endCreateDt = int(nowdate) #오늘시간으로 자동업데이트
+endCreateDt = int(nowdate)  # 오늘시간으로 자동업데이트
 
 url = f'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey={ServiceKey}&pageNo={pageNo}&numOfRows={numOfRows}&startCreateDt={startCreateDt}&endCreateDt={endCreateDt} '
 r = urllib.request.urlopen(url)
 result = r.read()
 soup = BeautifulSoup(result, 'lxml-xml')
-list = ['gubun','gubunCn','incDec']
-for items in soup.find_all(list):
 
-    print(items.text)
-    print("-----")#구분선
+# 리스트
 
 
+gubun_list = []  # 지역 이름
+defCnt_list = []  # 누적 확진자
+incDec_list = []  # 신규 확진자
+
+# column
+
+gubun = soup.find_all('gubun')
+defCnt = soup.find_all('defCnt')
+incDec = soup.find_all('incDec')
+
+for items in gubun:
+    gubun_list.append(items.text)
+
+for items in defCnt:
+    defCnt_list.append(items.text)
+
+for items in incDec:
+    incDec_list.append(items.text)
