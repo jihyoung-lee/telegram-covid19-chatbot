7078bbef8f6e8f7c7b636637c90f2f48b372e2c7
J.H.Lee <67559886+jihyoung-lee@users.noreply.github.com>
2020-12-29 23:28:28 +0900

    Update bot.py
    지역 별 확진자 수 / 총 확진자 수 구현

--- telegram_Chatbot/bot.py
+++ telegram_Chatbot/bot.py
@@ -7,7 +7,8 @@ import time
 import logging
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, CallbackContext
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, update
+from Toyproject.telegram_Chatbot.data import gubun_list, incDec_list, defCnt_list, nowdate2
 
 # bot info
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -17,70 +18,53 @@ logger = logging.getLogger(__name__)
 token = ""
 
 
-# start message
+# /start message
 def start(update, context):
     # username
     print(update.message.chat.username)
-    t = "%s! please click /test" % update.message.chat.first_name  # username
+    t = '''
+%s님 안녕하세요?
+지역별 코로나 확진자 수를
+알려주는 챗봇입니다.
+전국 확진자 수는 /total
+도움말은 /help 를 눌러주세요.
+    ''' % update.message.chat.first_name  # username
     context.bot.send_message(chat_id=update.message.chat_id, text=t)
 
 
-"""
-# echo
-def echo(update, context):
+# /help  message
+def help(update, context):
     print(update.message.chat.username)
-    t = "error"
-    context.bot.send_message(chat_id=update.message.chat_id, text=t)
-"""
-
-
-# buttons
-"""
-def button(buttons, n_cols, header_buttons=None, footer_buttons=None):
-    for i in range(0, len(buttons), n_cols):
-        menu = [buttons[i:i + n_cols]]
-        if header_buttons:
-            menu.insert(0, header_buttons)
-        if footer_buttons:
-            menu.append(footer_buttons)
-        return menu
-
-"""
-# /test  message
-def test(update, context):
-    print(update.message.chat.username)
-    t = "test!"
+    t = '''
+지역을 입력해주시면 
+오늘의 확진자 수를 알려드립니다
+이 챗봇은 광역시 / 도 단위만 지원합니다.
+ex ) 광주 , 서울 , 전남 , 제주
+    '''
     context.bot.send_message(chat_id=update.message.chat_id, text=t)
     time.sleep(0.3)
-    t1 = "test 1 or test 2"
-    context.bot.send_message(chat_id=update.message.chat_id, text=t1)
+
+
+# 지역별 확진자 수
+def get_message(update, context):
+    msg = update.message.text
+
+    for i in range(0, 18):
+        if msg == gubun_list[i]:
+            t = nowdate2 + '\n지역 : ' + gubun_list[i] + '\n신규 확진자 수 : ' + incDec_list[i] + ' 명\n누적 확진자 수 : ' + \
+                defCnt_list[i] + ' 명'
+            context.bot.send_message(chat_id=update.message.chat_id, text=t)
+            return
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text="/help 도움말을 참고하세요")
+
+
+# 총 확진자 수
+def total(update, context):
+    print(update.message.chat.username)
+    t = nowdate2 + '의 \n전체 신규 확진자 수는 ' + incDec_list[18] + ' 명.\n누적 확진자 수는 ' + defCnt_list[18] + ' 명입니다.'
+    context.bot.send_message(chat_id=update.message.chat_id, text=t)
     time.sleep(0.3)
-    """
-    show_list = [InlineKeyboardButton("test1", callback_data="test1"),
-                 InlineKeyboardButton("test2", callback_data="test2")]
-
-    show_markup = InlineKeyboardMarkup(button(show_list, len(show_list) - 1))
-
-    reply_markup = InlineKeyboardMarkup(show_list)
-    query.edit_message_text(
-        text="Choose a route", reply_markup=reply_markup
-    )
-
-    # context.bot.send_message("select", reply_markup=show_markup)
-    """
-
-
-# callback
-def callback_get(update, context):
-    print("callback")
-    if update.callback_query.data == "test1":
-        context.edit_message_text(text="select test1",
-                                  chat_id=update.callback_query.message.chat_id,
-                                  message_id=update.callback_query.message.message_id)
-    if update.callback_query.data == "test2":
-        context.edit_message_text(text="select test2",
-                                  chat_id=update.callback_query.message.chat_id,
-                                  message_id=update.callback_query.message.message_id)
 
 
 # error
@@ -88,20 +72,17 @@ def error_handler(update: Update, context: CallbackContext) -> None:
     logger.error(msg="Exception while handling an update:", exc_info=context.error)
 
 
-# def error(update, context, error):
-#     logger.warning('Update "%s" caused error "%s"', context, error)
-
-
-# main function
 def main():
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-    # dispatcher.add_handler(MessageHandler(Filters.text, echo))
-    test_handler = CommandHandler('test', test)
-    dispatcher.add_handler(test_handler)
-    dispatcher.add_handler(CallbackQueryHandler(callback_get))
+    total_handler = CommandHandler('total', total)
+    dispatcher.add_handler(total_handler)
+    help_handler = CommandHandler('help', help)
+    dispatcher.add_handler(help_handler)
+    message_handler = MessageHandler(Filters.text, get_message)
+    dispatcher.add_handler(message_handler)
 
     # log all errors
     dispatcher.add_error_handler(logging.error)
